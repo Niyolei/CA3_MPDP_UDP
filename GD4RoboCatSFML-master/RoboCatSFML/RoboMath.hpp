012636c34fc600e8c7ad552b52b57a2be42e7632
Gracie Chaudhary <94973395+graciechaudhary@users.noreply.github.com>
2025-05-14 16:25:14 +0100

    Add more colours
    Adding more colours to choose from

--- GD4RoboCatSFML-master/RoboCatSFML/RoboMath.hpp
+++ GD4RoboCatSFML-master/RoboCatSFML/RoboMath.hpp
@@ -197,9 +197,34 @@ namespace Colors
 	static const Vector3 Red(255.0f, 0.0f, 0.0f);
 	static const Vector3 Green(0.0f, 255.0f, 0.0f);
 	static const Vector3 Blue(0.0f, 0.0f, 255.0f);
-	static const Vector3 LightYellow(255.0f, 255.0f, 224.4f);
-	static const Vector3 LightBlue(173.4f, 216.75f, 229.5f);
-	static const Vector3 LightPink(255.0f, 181.05f, 193.8f);
-	static const Vector3 LightGreen(142.8f, 237.15f, 142.8f);
+	static const Vector3 Yellow(255.0f, 255.0f, 0.0f);
+	static const Vector3 Cyan(0.0f, 255.0f, 255.0f);
+	static const Vector3 Magenta(255.0f, 0.0f, 255.0f);
+	static const Vector3 Orange(255.0f, 165.0f, 0.0f);
+
+	static const Vector3 Lime(50.0f, 205.0f, 50.0f);
+	static const Vector3 Pink(255.0f, 192.0f, 203.0f);
+	static const Vector3 Purple(128.0f, 0.0f, 128.0f);
+	static const Vector3 Brown(139.0f, 69.0f, 19.0f);
+	static const Vector3 SkyBlue(135.0f, 206.0f, 235.0f);
+	static const Vector3 DarkGreen(0.0f, 100.0f, 0.0f);
+	static const Vector3 LightGray(211.0f, 211.0f, 211.0f);
+	static const Vector3 DarkGray(105.0f, 105.0f, 105.0f);
+	static const Vector3 Gold(255.0f, 215.0f, 0.0f);
+	static const Vector3 Navy(0.0f, 0.0f, 128.0f);
+	static const Vector3 Teal(0.0f, 128.0f, 128.0f);
+
+	static const Vector3 Salmon(250.0f, 128.0f, 114.0f);
+	static const Vector3 Coral(255.0f, 127.0f, 80.0f);
+	static const Vector3 Tomato(255.0f, 99.0f, 71.0f);
+	static const Vector3 Olive(128.0f, 128.0f, 0.0f);
+	static const Vector3 Aqua(0.0f, 255.0f, 255.0f);
+	static const Vector3 Indigo(75.0f, 0.0f, 130.0f);
+	static const Vector3 Plum(221.0f, 160.0f, 221.0f);
+	static const Vector3 Turquoise(64.0f, 224.0f, 208.0f);
+	static const Vector3 Mint(189.0f, 252.0f, 201.0f);
+	static const Vector3 Lavender(230.0f, 230.0f, 250.0f);
+
+
 }
 
